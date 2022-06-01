57954466fd008f9ba8bf7d6b62c6c636c3b66d05
Sergey Trofimov <sergey@trofimov.name>
2022-06-01 15:31:07 +0200

    Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/caffeine-memoize "1.0.2"
+(defproject com.github.strojure/caffeine-memoize "1.1.3-SNAPSHOT"
   :description "Clojure function memoization using Caffeine java library"
   :url "https://github.com/strojure/caffeine-memoize"
   :license {:name "Apache License 2.0"
