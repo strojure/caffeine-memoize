57ade775d11ebd8ceedc0f107f0d203a13091223
Sergey Trofimov <sergey@trofimov.name>
2023-03-08 17:25:14 +0100

    (chore) Change license to Unlicense

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## `1.1.5-SNAPSHOT`
+
+Release date `UNRELEASED`
+
+- (chore) Change license to Unlicense.
+
+## `1.0.4`
+
+Release date `2022-06-02`
--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject com.github.strojure/caffeine-memoize "1.1.5-SNAPSHOT"
   :description "Clojure function memoization using Caffeine java library"
   :url "https://github.com/strojure/caffeine-memoize"
-  :license {:name "Apache License 2.0"
-            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
+  :license {:name "Unlicense" :url "https://unlicense.org"}
 
   :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
                  [com.github.ben-manes.caffeine/caffeine "3.1.1"]]
