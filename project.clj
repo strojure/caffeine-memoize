e79b66cddedd70ea0fdd7380e866985a47a6b278
Sergey Trofimov <sergey@trofimov.name>
2023-03-08 19:15:40 +0100

    (style) Use "The Unlicense" license name

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.github.strojure/caffeine-memoize "1.0.10-SNAPSHOT"
   :description "Clojure function memoization using Caffeine java library"
   :url "https://github.com/strojure/caffeine-memoize"
-  :license {:name "Unlicense" :url "https://unlicense.org"}
+  :license {:name "The Unlicense" :url "https://unlicense.org"}
 
   :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
                  [com.github.ben-manes.caffeine/caffeine "3.1.5"]]
